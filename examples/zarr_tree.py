8139440d3e1238540cc24ef451a3f67e3ff99758
Davis Bennett <davis.v.bennett@gmail.com>
2022-09-14 00:31:52 -0400

    Update zarr_tree.py

--- examples/zarr_tree.py
+++ examples/zarr_tree.py
@@ -4,9 +4,9 @@ from rich import print_json
 from ztree.parser import visit
 
 zgroup = zarr.open(zarr.MemoryStore())
-zgroup.attrs['foo'] = 100 # this will fail validation
+zgroup.attrs['foo'] = 100
 zgroup.create_dataset(name='my_array', shape=(10,))
-zgroup['my_array'].attrs['bar'] = '10' # this will fail validation
+zgroup['my_array'].attrs['bar'] = '10'
 
 if __name__ == '__main__':
     name, tree = visit(zgroup)
